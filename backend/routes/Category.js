bc6820dc4a9923f4081792a967bfee62e057d399
s3877653 <hotranminhkhoi@gmail.com>
2023-09-09 15:09:00 +0700

    reorganize folder

--- backend/routes/Category.js
+++ backend/routes/Category.js
@@ -1,5 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const Category = require("../model/Category");
+const Category = require("../CategoryModel");
 
 
+// router.get("/",getCategoryList)
+// router.post("/add",addCategoy)
+
+async function getCategoryList(req,res){
+    try{
+        const categories = await Category.find()
+        res.status(200).send(categories)
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
+
+async function addCategoy(req,res){
+    try {
+        const category = req.body
+        await Category.create(category)
+        res.sendStatus(201)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
+module.exports = router
--- backend/routes/Index.js
+++ backend/routes/Index.js
@@ -1,10 +1,12 @@
 const Seller = require('./Seller');
 const WarehouseAdmin = require('./WarehouseAdmin')
+const Category = require("./Category")
 
 function route(app) {
     
     app.use('/seller', Seller);
     app.use('/warehouseAdmin',WarehouseAdmin)
+    app.use('/Category',Category)
 }
 
 module.exports = route;
