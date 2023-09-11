32527f5c3be080e2f7bb2c889d0b7302a53da70c
s3877653 <hotranminhkhoi@gmail.com>
2023-09-11 16:47:36 +0700

    add customer

---
+++ frontend/customer.js
@@ -0,0 +1,24 @@
+getCustomerProduct()
+
+async function getCustomerProduct(){
+    const res = await fetch("http://127.0.0.1:2222/customer/", { mode: "cors" });
+    const productList = await res.json();
+    let productListHtml = productList.map(product => `<div class="product-item">
+    <div class="product-item-left">
+        <img src="${product.imgString}">
+    </div>
+    <div class="product-item-middle">
+        <p>Product name:<b>${product.title}</b></p>
+        <p>Description:<b>${product.description}</b></p>
+    </div>
+    <div class="product-item-right">
+        <p>Price:<b>${product.price}</b></p>
+        <p>Dimension:<b>l:${product.length}, w:${product.width}, h:${product.height}</b></p>
+    </div>
+    <div class="product-item-btn">
+        <a href="sellerEditProduct.html">edit</a>
+        <button onclick="deleteProduct(${product.id})">delete</button>
+    </div>
+</div>`)
+    document.querySelector(".product-list").innerHTML = productListHtml.join("\n")
+}
